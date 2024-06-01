b5770aa9536c6f070833a9490ff08463008b9d08
amaanlari <amaanlari@outlook.comm>
2024-06-01 12:19:31 +0530

    initialized project 2

---
+++ backend-project2/top-products-service/src/main/java/com/lari/topproductsservice/TopProductsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lari.topproductsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopProductsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopProductsServiceApplication.class, args);
+    }
+
+}
---
+++ backend-project2/top-products-service/src/test/java/com/lari/topproductsservice/TopProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lari.topproductsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopProductsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
